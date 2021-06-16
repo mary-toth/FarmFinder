42b4394a54d86a929f10c42c1e603c9ef319e0e4
Mary Toth <maryarylynn@gmail.com>
2021-06-16 14:36:29 -0400

    added two example farms with sql

---
+++ Models/exampledata.sql
@@ -0,0 +1,5 @@
+TRUNCATE TABLE "Farms";
+
+INSERT INTO "Farms" ("Name", "Address", "Phone", "Description", "Organic", "Meat", "Eggs", "Dairy") VALUES ('Cool Farm', '123 Main St', '123-456-7890', 'Very cool awesome farm with lots of veggies and yummy things', False, True, True, True);
+
+INSERT INTO "Farms" ("Name", "Address", "Phone", "Description", "Organic", "Meat", "Eggs", "Dairy") VALUES ('Anotha Farm', '867 Cow St', '321-542-4521', 'Farm farm farm cool stuff awesome', True, True, True, True);
